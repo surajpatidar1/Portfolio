254d1b4ac99fafc0fa5e154253f82cf31ce29913
Suraj Patidar <167690910+surajpatidar1@users.noreply.github.com>
2025-05-24 12:36:18 +0530

    form functionality

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
 Hi, I'm Suraj from Khandwa, Madhya Pradesh. <br />I completed my 10th and 12th grade from Sophia Convent School, Khandwa. <br /> Currently, I am pursuing a Bachelor of Technology (B.Tech) from Shri Vaishnav Vidyapeeth Vishwavidyalaya, Indore.
 I have a keen interest in web development and have recently explored the MERN stack (MongoDB, Express.js, React.js, and Node.js). <br /> Though I'm currently at a beginner level, I have a basic understanding of how the technologies in the MERN stack work together to build full-stack web applications.
 I am passionate about learning new technologies and constantly improving my skills to become a proficient developer.
-        <button className='flex  bg-slate-800 text-white p-2 rounded-lg  mt-5'> <a href="#Skills" className='flex items-center gap-1'>Skills<FaChevronUp /></a></button>
+        <button className='flex  bg-slate-800 text-white p-2 rounded-lg  mt-5'> <a href="/resume.pdf" download="Suraj_Patidar_Resume.pdf" className='flex items-center text-sm '>Download Resume</a></button>
       </p>
       
     </div>
